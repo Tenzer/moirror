72836899d8a201f59890f83dd15c6036c3f0b88a
Jeppe Toustrup <tenzer@tenzer.dk>
2012-07-01 23:42:32 +0200

    Lots of work done... It all basically works now, the only things needed are: - Adding of IPv4/IPv6 records for contries which doesn't have those records - Writing the generated config to a file (could perhaps just use stdout) - Code clean up, should probably move some more bits to methods.js - Possibly add support for aliases

--- app.js
+++ app.js
@@ -1,5 +1,80 @@
 var mirrors = require(__dirname + '/mirrors.json');
 var methods = require(__dirname + '/lib/methods.js');
+var async = require('async');
 
-var enabled_mirrors = mirrors.list.filter(methods.isMirrorEnabled);
-console.log(enabled_mirrors);
+// Filter out mirrors which aren't enabled
+async.filter(mirrors.list, methods.isMirrorEnabled, processEnabled);
+
+// Generate a list of mirror addresses along with their country
+function processEnabled (list) {
+  async.map(list, methods.splitProtocols, flattenResults);
+}
+
+// Put mirrors into one array, one address per item
+function flattenResults (err, list) {
+  if (err) {
+    console.error('Error:', err);
+  } else {
+    async.concat(list, methods.flattenArray, testMirrors);
+    //testMirrors(methods.flattenArray(list));
+  }
+}
+
+
+function testMirrors (err, list) {
+  if (err) {
+    console.error('Error:', err);
+  } else {
+    //async.filter(list, methods.testMirror, generateResultObject);
+    generateResultObject(list);
+  }
+}
+
+
+function generateResultObject (list) {
+  function addRecord (item) {
+    if (!all.hasOwnProperty(item.record)) {
+      all[item.record] = [];
+    }
+
+    all[item.record].push([item.hostname, 10]);
+
+    if (!regions.hasOwnProperty(item.country)) {
+      regions[item.country] = {};
+    }
+
+    if (!regions[item.country].hasOwnProperty(item.record)) {
+      regions[item.country][item.record] = [];
+    }
+
+    regions[item.country][item.record].push([item.hostname, 10]);
+  }
+
+  var all = {
+    a: [],
+    aaaa: []
+  };
+
+  var regions = {};
+
+  for (var i = 0; i < list.length; i++) {
+    addRecord(list[i]);
+  }
+
+  var result = {
+    serial: methods.serialDate(),
+    data: {
+      '': all
+    }
+  };
+
+  for (var prop in regions) {
+    result.data[prop] = regions[prop];
+  }
+
+  outputJson(result);
+}
+
+function outputJson (obj) {
+  console.log(JSON.stringify(obj, null, 4));
+}
