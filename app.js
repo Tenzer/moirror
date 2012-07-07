a5bf1fa33c3d216d1993089b4fbdd695f9c63288
Jeppe Toustrup <tenzer@tenzer.dk>
2012-07-07 15:42:45 +0200

    Converted shell script to webserver

--- app.js
+++ app.js
@@ -1,239 +1,258 @@
-if (!process.argv[2]) {
-  console.error('Error: You need to supply the filename to the configuration as an argument.');
-  process.exit(1);
-}
-
-var config = require('./' + process.argv[2]); // Could most likely be made better
-var package_json = require(__dirname + '/package.json');
-var async = require('async');
+var restify = require('restify');
+var fs = require('fs');
 var util = require('util');
+var async = require('async');
 var http = require('http');
 
+var server = restify.createServer({
+  name: 'moirror'
+});
 
-// Filter out mirrors which aren't enabled
-async.filter(config.mirrors, isMirrorEnabled, processEnabled);
-
-  function isMirrorEnabled (item, callback) {
-    if (item.hasOwnProperty('enabled')) {
-      callback(item.enabled);
-    } else {
-      callback(false);
-    }
-  }
-
-
-// Generate a list of mirror addresses along with their country
-function processEnabled (list) {
-  async.map(list, splitProtocols, flattenResults);
-}
-
-  function splitProtocols (item, callback) {
-    var result = [];
-
-    if (item.hasOwnProperty('ipv4')) {
-      result.push({
-        country: item.country,
-        hostname: item.ipv4,
-        record: 'a',
-        priority: item.priority
-      });
+server.get('/', function (req, res, next) {
+  fs.readdir(__dirname + '/configs/', function (err, files) {
+    if (err) {
+      res.send(500, util.format('Error when getting files in config directory: %s', err));
+      return next();
     }
 
-    if (item.hasOwnProperty('ipv6')) {
-      result.push({
-        country: item.country,
-        hostname: item.ipv6,
-        record: 'aaaa',
-        priority: item.priority
-      });
+    res.send(200, files);
+    return next();
+  });
+});
+
+server.get('/:config', function (req, res, next) {
+  fs.readFile(__dirname + '/configs/' + req.params.config, function (err, data) {
+    if (err) {
+      res.send(500, 'Error: ' + err.toString());
+      return next();
     }
 
-    callback(null, result);
-  }
+    var config = JSON.parse(data);
 
+    // Filter out mirrors which aren't enabled
+    async.filter(config.mirrors, isMirrorEnabled, processEnabled);
 
-// Put mirrors into one array, one address per item
-function flattenResults (err, list) {
-  if (err) {
-    console.error('Error:', err);
-  } else {
-    async.concat(list, flattenArray, testMirrors);
-  }
-}
+    function isMirrorEnabled (item, callback) {
+      if (item.hasOwnProperty('enabled')) {
+        callback(item.enabled);
+      } else {
+        callback(false);
+      }
+    }
 
-  function flattenArray (item, callback) {
-    if (util.isArray(item)) {
+    // Generate a list of mirror addresses along with their country
+    function processEnabled (list) {
+      async.map(list, splitProtocols, flattenResults);
+    }
+
+    function splitProtocols (item, callback) {
       var result = [];
 
-      for (var i = 0; i < item.length; i++) {
-        result.push(item[i]);
+      if (item.hasOwnProperty('ipv4')) {
+        result.push({
+          country: item.country,
+          hostname: item.ipv4,
+          record: 'a',
+          priority: item.priority
+        });
       }
 
-      return callback(null, result);
-    } else {
-      return callback(null, item);
+      if (item.hasOwnProperty('ipv6')) {
+        result.push({
+          country: item.country,
+          hostname: item.ipv6,
+          record: 'aaaa',
+          priority: item.priority
+        });
+      }
+
+      callback(null, result);
     }
-  }
 
 
-
-function testMirrors (err, list) {
-  if (err) {
-    console.error('Error:', err);
-  } else {
-    async.filter(list, testMirror, generateResultObject);
-  }
-}
-
-  function testMirror (item, callback) {
-    item.path = config.path;
-    item.headers = {
-      Host: config.hostname,
-      'User-Agent': 'moirror/' + package_json.version +
-        ' (Node/' + process.versions.node +
-        '; +https://github.com/Tenzer/moirror)'
-    };
-
-    var req = http.get(item, function (res) {
-      if (res.statusCode !== 200) {
-        console.error(util.format('Disabled %s, gave status code: %d', item.hostname, res.statusCode));
-        return callback(false);
+    // Put mirrors into one array, one address per item
+    function flattenResults (err, list) {
+      if (err) {
+        res.send(500, 'Error: ' + err.toString());
+        return next();
+      } else {
+        async.concat(list, flattenArray, testMirrors);
       }
+    }
 
-      var data = '';
-      res.on('data', function (chunk) {
-        data = data + chunk;
-      });
+    function flattenArray (item, callback) {
+      if (util.isArray(item)) {
+        var result = [];
 
-      res.on('end', function () {
-        if (config.expected_bytes && data.length !== config.expected_bytes) {
-          console.error(
-            util.format('Disabled %s, gave incorrect amount of data: %d bytes (expected %d bytes)',
-              item.hostname,
-              data.length,
-              config.expected_bytes
-            )
-          );
-          return callback(false);
-        } else {
-          return callback(true);
+        for (var i = 0; i < item.length; i++) {
+          result.push(item[i]);
         }
+
+        return callback(null, result);
+      } else {
+        return callback(null, item);
+      }
+    }
+
+
+    function testMirrors (err, list) {
+      if (err) {
+        res.send(500, 'Error: ' + err.toString());
+        return next();
+      } else {
+        async.filter(list, testMirror, generateResultObject);
+      }
+    }
+
+    function testMirror (item, callback) {
+      item.path = config.path;
+      item.headers = {
+        Host: config.hostname,
+        'User-Agent': 'moirror/0.0.0' +
+          ' (Node/' + process.versions.node +
+          '; +https://github.com/Tenzer/moirror)'
+      };
+
+      var req = http.get(item, function (res) {
+        if (res.statusCode !== 200) {
+          console.error(util.format('Disabled %s, gave status code: %d', item.hostname, res.statusCode));
+          return callback(false);
+        }
+
+        var data = '';
+        res.on('data', function (chunk) {
+          data = data + chunk;
+        });
+
+        res.on('end', function () {
+          if (config.expected_bytes && data.length !== config.expected_bytes) {
+            console.error(
+              util.format('Disabled %s, gave incorrect amount of data: %d bytes (expected %d bytes)',
+                item.hostname,
+                data.length,
+                config.expected_bytes
+              )
+            );
+            return callback(false);
+          } else {
+            return callback(true);
+          }
+        });
+
+        res.on('error', function (err) {
+          console.error(util.format('Disabled %s, gave error: %s', item.hostname, err));
+          return callback(false);
+        });
       });
 
-      res.on('error', function (err) {
-        console.error(util.format('Disabled %s, gave error: %s', item.hostname, err));
+      req.on('error', function (err) {
+        // This error event is fired when a mirror is timed out, and the script is ending
         return callback(false);
       });
-    });
 
-    req.on('error', function (err) {
-      // This error event is fired when a mirror is timed out, and the script is ending
-      return callback(false);
-    });
-
-    req.on('socket', function (socket) {
-      socket.setTimeout(config.timeout);
-      socket.on('timeout', function () {
-        console.error(util.format('Disabled %s, timed out.', item.hostname));
-        req.abort();
-        return callback(false);
+      req.on('socket', function (socket) {
+        socket.setTimeout(config.timeout);
+        socket.on('timeout', function () {
+          console.error(util.format('Disabled %s, timed out.', item.hostname));
+          req.abort();
+          return callback(false);
+        });
       });
-    });
-  }
-
-
-function generateResultObject (list) {
-  function addRecord (item) {
-    if (!all.hasOwnProperty(item.record)) {
-      all[item.record] = [];
     }
 
-    all[item.record].push([item.hostname, item.priority]);
 
-    if (!regions.hasOwnProperty(item.country)) {
-      regions[item.country] = {};
+    function generateResultObject (list) {
+      function addRecord (item) {
+        if (!all.hasOwnProperty(item.record)) {
+          all[item.record] = [];
+        }
+
+        all[item.record].push([item.hostname, item.priority]);
+
+        if (!regions.hasOwnProperty(item.country)) {
+          regions[item.country] = {};
+        }
+
+        if (!regions[item.country].hasOwnProperty(item.record)) {
+          regions[item.country][item.record] = [];
+        }
+
+        regions[item.country][item.record].push([item.hostname, item.priority]);
+      }
+
+      var all = {
+        a: [],
+        aaaa: []
+      };
+
+      var regions = {};
+
+      for (var i = 0; i < list.length; i++) {
+        addRecord(list[i]);
+      }
+
+      // Fill regions with the opposite A or AAAA records if missing
+      for (var region in regions) {
+        if (!regions[region].hasOwnProperty('a')) {
+          regions[region].a = all.a;
+        } else if (!regions[region].hasOwnProperty('aaaa')) {
+          regions[region].aaaa = all.aaaa;
+        }
+      }
+
+      var result = {
+        serial: serialDate(),
+        data: {
+          '': all
+        }
+      };
+
+      for (var prop in regions) {
+        result.data[prop] = regions[prop];
+      }
+
+      res.send(200, result);
+      return next();
     }
 
-    if (!regions[item.country].hasOwnProperty(item.record)) {
-      regions[item.country][item.record] = [];
+    function serialDate () {
+      var result;
+      var date = new Date();
+
+      // Year
+      result = date.getUTCFullYear().toString();
+
+      // Month
+      var month = (date.getUTCMonth() + 1).toString();
+      if (month.length === 1) {
+        month = '0' + month;
+      }
+      result += month;
+
+      // Date/Day
+      var day = date.getUTCDate().toString();
+      if (day.length === 1) {
+        day = '0' + day;
+      }
+      result += day;
+
+      // Hours
+      var hour = date.getUTCHours().toString();
+      if (hour.length === 1) {
+        hour = '0' + hour;
+      }
+      result += hour;
+
+      // Minutes
+      var minute = date.getUTCMinutes().toString();
+      if (minute.length === 1) {
+        minute = '0' + hour;
+      }
+      result += minute;
+
+      return result;
     }
+  });
+});
 
-    regions[item.country][item.record].push([item.hostname, item.priority]);
-  }
-
-  var all = {
-    a: [],
-    aaaa: []
-  };
-
-  var regions = {};
-
-  for (var i = 0; i < list.length; i++) {
-    addRecord(list[i]);
-  }
-
-  // Fill regions with the opposite A or AAAA records if missing
-  for (var region in regions) {
-    if (!regions[region].hasOwnProperty('a')) {
-      regions[region].a = all.a;
-    } else if (!regions[region].hasOwnProperty('aaaa')) {
-      regions[region].aaaa = all.aaaa;
-    }
-  }
-
-  var result = {
-    serial: serialDate(),
-    data: {
-      '': all
-    }
-  };
-
-  for (var prop in regions) {
-    result.data[prop] = regions[prop];
-  }
-
-  outputJson(result);
-}
-
-  function serialDate () {
-    var result;
-    var date = new Date();
-
-    // Year
-    result = date.getUTCFullYear().toString();
-
-    // Month
-    var month = (date.getUTCMonth() + 1).toString();
-    if (month.length === 1) {
-      month = '0' + month;
-    }
-    result += month;
-
-    // Date/Day
-    var day = date.getUTCDate().toString();
-    if (day.length === 1) {
-      day = '0' + day;
-    }
-    result += day;
-
-    // Hours
-    var hour = date.getUTCHours().toString();
-    if (hour.length === 1) {
-      hour = '0' + hour;
-    }
-    result += hour;
-
-    // Minutes
-    var minute = date.getUTCMinutes().toString();
-    if (minute.length === 1) {
-      minute = '0' + hour;
-    }
-    result += minute;
-
-    return result;
-  }
-
-
-function outputJson (obj) {
-  console.log(JSON.stringify(obj, null, 4));
-}
+server.listen(8001);
